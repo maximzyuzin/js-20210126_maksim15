6bf0b8cd181510c254adafb93ca4d95d3490c8e9
Maxim Zyuzin <m.s.zyuzin@ya.ru>
2021-02-01 02:38:55 +0300

    function omit(obj, fields)

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,34 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+    let newObj = {};
+    let arrs = Object.entries(obj);
 
-};
+    let i = 0;
+    let j = 0;
+
+    while (i < arrs.length) {
+        while (j <= fields.length) {
+            if (fields[j] === arrs[i][0]) {
+                arrs.splice(i, 1);
+                if (fields.length - 1 === j) {
+                    break;
+                } else {
+                    j = j + 1;
+                    break;
+                }
+            } else {
+                i = i + 1;
+                break;
+            }
+        }
+    }
+
+    if (arrs.length !== 0) {
+        for (let k = 0; k < arrs.length; k++) {
+            newObj[arrs[k][0]] = arrs[k][1];
+        }
+    }
+
+    return newObj;
+}

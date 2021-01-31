941379ed103e4c6860700e2a6e6fde78dd94182b
Maxim Zyuzin <m.s.zyuzin@ya.ru>
2021-01-31 19:36:51 +0300

    function sortStrings(arr, param)

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,34 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    let sortArr = [...arr];
+    let lenArr = arr.length;
 
+    for (let i = 0; i < lenArr - 1; i++) {
+        for (let j = 0; j < lenArr - 1 - i; j++) {
+
+            let strLower1 = sortArr[j].toLowerCase();
+            let strLower2 = sortArr[j + 1].toLowerCase();
+
+            if (strLower1.localeCompare(strLower2) === 0) {
+                if (sortArr[j].localeCompare(sortArr[j + 1]) < 0) {
+                    let temp = sortArr[j + 1];
+                    sortArr[j + 1] = sortArr[j];
+                    sortArr[j] = temp;
+                }
+            } else {
+                if (sortArr[j].localeCompare(sortArr[j + 1]) > 0) {
+                    let temp = sortArr[j + 1];
+                    sortArr[j + 1] = sortArr[j];
+                    sortArr[j] = temp;
+                }
+            }
+        }
+    }
+
+    if (param == 'desc') {
+        sortArr = sortArr.reverse();
+    }
+
+    return sortArr;
 }

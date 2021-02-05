0c7515ddce3ce1afff8a9f0dab292545d5c479fc
Maxim Zyuzin <m.s.zyuzin@ya.ru>
2021-02-05 13:12:20 +0300

    function createGetter(path)

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    const pathArr = path.split('.');
 
+    return function (obj) {
+        let value = null;
+
+        for (const item of pathArr) {
+            value = obj[item];
+            if (value === undefined) {
+                return;
+            }
+            obj = value;
+        }
+        return value;
+    };
 }

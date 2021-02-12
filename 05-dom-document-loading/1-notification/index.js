dcdd947a060024e36d3ef5a70864cd6f490976df
Maxim Zyuzin <m.s.zyuzin@ya.ru>
2021-02-12 16:07:17 +0300

    class NotificationMessage {}

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,54 @@
 export default class NotificationMessage {
+    static component = null;
 
+    constructor(message = '', {
+        duration = 0,
+        type = ''
+    } = {}) {
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+
+        this.render();
+    }
+
+    render() {
+        const element = document.createElement('DIV');
+        element.innerHTML = this.template;
+        this.element = element.firstElementChild;
+    }
+
+    get template() {
+        return `
+        <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+                </div>
+        </div>
+        `;
+    }
+
+    show(targetElement) {
+        if (NotificationMessage.component) NotificationMessage.component.remove();
+
+        if (targetElement) targetElement.append(this.element);
+        else document.body.append(this.element);
+
+        NotificationMessage.component = this.element;
+
+        setTimeout(() => this.remove(), this.duration);
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+        NotificationMessage.component = null;
+    }
 }
